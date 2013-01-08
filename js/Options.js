789056f110fa118118e826620590b4af8b38d63d
Steffen Persch <s.persch@unlooped.com>
2013-01-08 04:35:36 +0100

    [ENH] setOptions now supports multiple arguments

--- js/Options.js
+++ js/Options.js
@@ -1,12 +1,14 @@
 "use strict";
 
 var prime = require('prime');
+var object = require('prime-util/types/object');
 
 var Options = prime({
     
     setOptions: function(options) {
-        var object = require('prime-util/types/object');
-        this.options = object.merge({}, this.options);
+        var args = [{}, this.options];
+        args.push.apply(args, arguments);
+        this.options = object.merge.apply(null, args);
         
         if (!options) {
             return;
